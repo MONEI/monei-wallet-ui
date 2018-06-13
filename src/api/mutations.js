68e1afe00f4a6dc0deb95c112312dd169990466c
Dmitriy Nevzorov <dmitriy.nevzorov@microapps.com>
2018-06-13 19:01:12 +0300

    add income boolean to schema

--- src/api/mutations.js
+++ src/api/mutations.js
@@ -10,6 +10,7 @@ export const NewTransactionMutation = gql`
       status
       createdAt
       updatedAt
+      income
     }
   }
 `;
--- src/api/queries.js
+++ src/api/queries.js
@@ -16,6 +16,7 @@ export const UserDataQuery = gql`
       status
       createdAt
       updatedAt
+      income
     }
   }
 `;
