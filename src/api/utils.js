a10a7f50cf8c25fb15ebfa687cac820f4d4e95fd
Dmitriy Nevzorov <dmitriy.nevzorov@microapps.com>
2018-07-03 19:22:39 +0300

    - fix update transaction notification crashing the app

--- src/api/utils.js
+++ src/api/utils.js
@@ -4,17 +4,17 @@ import {TransactionsQuery, UserQuery} from './queries';
 /**
  * Creates transaction in apollo cache
  * @param client - apollo client
- * @param transaction
+ * @param data - transaction
  */
-export const createLocalTransaction = (client, transaction) => {
+export const createLocalTransaction = (client, data) => {
+  const transaction = {__typename: 'Transaction', toInfo: '', ...data};
   try {
     const transactionsData = client.readQuery({query: TransactionsQuery, variables: {from: 0}});
-    const newTrx = {__typename: 'Transaction', toInfo: '', ...transaction};
     client.writeQuery({
       query: TransactionsQuery,
       variables: {from: 0},
       data: produce(transactionsData, draft => {
-        draft.transactions.items = [newTrx, ...draft.transactions.items];
+        draft.transactions.items = [transaction, ...draft.transactions.items];
       })
     });
   } catch (_) {}
@@ -23,9 +23,10 @@ export const createLocalTransaction = (client, transaction) => {
 /**
  * Updates transaction and user balance in apollo cache,
  * @param client - apollo client
- * @param transaction
+ * @param data - transaction
  */
-export const updateLocalTransaction = (client, transaction) => {
+export const updateLocalTransaction = (client, data) => {
+  const transaction = {__typename: 'Transaction', toInfo: '', ...data};
   try {
     const transactionsData = client.readQuery({query: TransactionsQuery, variables: {from: 0}});
     client.writeQuery({
