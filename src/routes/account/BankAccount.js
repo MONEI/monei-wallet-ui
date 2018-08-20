8808e7cd93d04109f3a455c1192b835fd2084f95
Dmitriy Nevzorov <dmitriy.nevzorov@microapps.com>
2018-08-20 17:26:33 +0300

    attach bank account form

--- src/routes/account/BankAccount.js
+++ src/routes/account/BankAccount.js
@@ -1,15 +1,61 @@
 import {Button, Card, Form, Input} from 'antd';
-import {compact} from 'lib/utils';
 import React, {Component, Fragment} from 'react';
+import {graphql} from 'react-apollo';
+import {AttachBankAccount} from 'api/mutations';
 
 class BankAccount extends Component {
+  state = {
+    isLoading: false,
+    isEditing: false
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    const {form} = this.props;
+    form.validateFields((err, data) => {
+      if (err) return;
+      this.props.attachBankAccount(data);
+    });
+  };
+
   render() {
+    const {isLoading, isEditing} = this.state;
+    const {getFieldDecorator} = this.props.form;
     return (
       <Card title="Bank Account">
-        <Button type="primary">Attach bank account</Button>
+        {isEditing ? (
+          <Form onSubmit={this.handleSubmit} layout="vertical">
+            <Form.Item label="Account holder name">
+              {getFieldDecorator('accountHolderName')(<Input />)}
+            </Form.Item>
+            <Form.Item label="Country">{getFieldDecorator('country')(<Input />)}</Form.Item>
+            <Form.Item label="IBAN">{getFieldDecorator('IBAN')(<Input />)}</Form.Item>
+            <Button onClick={() => this.setState({isEditing: false})}>Cancel</Button>{' '}
+            <Button loading={isLoading} type="primary" htmlType="submit">
+              Attach
+            </Button>
+          </Form>
+        ) : (
+          <Fragment>
+            <Button type="primary" onClick={() => this.setState({isEditing: true})}>
+              Attach bank account
+            </Button>
+          </Fragment>
+        )}
       </Card>
     );
   }
 }
 
-export default BankAccount;
+const BankAccountForm = Form.create()(BankAccount);
+
+export default graphql(AttachBankAccount, {
+  props: ({mutate}) => ({
+    attachBankAccount: data => {
+      console.log(data);
+      return mutate({
+        variables: data
+      });
+    }
+  })
+})(BankAccountForm);
--- src/routes/buy/Buy.js
+++ src/routes/buy/Buy.js
@@ -1,14 +1,16 @@
-import {Card} from 'antd';
+import {Card, Row, Col} from 'antd';
 import React from 'react';
 import BuyCoinsForm from './BuyCoinsForm';
 
 const Buy = ({prepareCheckout}) => {
   return (
-    <div>
-      <Card title="Buy coins" style={{maxWidth: 550}}>
-        <BuyCoinsForm onFormSubmit={prepareCheckout} />
-      </Card>
-    </div>
+    <Row>
+      <Col span={13}>
+        <Card title="Buy coins">
+          <BuyCoinsForm onFormSubmit={prepareCheckout} />
+        </Card>
+      </Col>
+    </Row>
   );
 };
 
