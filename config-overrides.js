eaa17aa2b5ffe8c22e6f18a6f0e8820f6fcc59c3
Dmitriy Nevzorov <dmitriy.nevzorov@microapps.com>
2018-09-11 15:59:10 +0300

    generate bundle report for production

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,5 @@
 const {injectBabelPlugin} = require('react-app-rewired');
+const rewireWebpackBundleAnalyzer = require('react-app-rewire-webpack-bundle-analyzer');
 const rewireLess = require('react-app-rewire-less');
 
 module.exports = function override(config, env) {
@@ -6,5 +7,11 @@ module.exports = function override(config, env) {
   config = rewireLess.withLoaderOptions({
     modifyVars: {'@primary-color': '#096dd9'}
   })(config, env);
+  if (env === 'production') {
+    config = rewireWebpackBundleAnalyzer(config, env, {
+      analyzerMode: 'static',
+      reportFilename: 'report.html'
+    });
+  }
   return config;
 };
